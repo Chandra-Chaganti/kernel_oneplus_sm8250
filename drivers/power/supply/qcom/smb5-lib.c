1f2699b271cabad931bfce5b7a83774de1b3315e
engstk <eng.stk@sapo.pt>
2020-06-19 16:50:32 +0100

    power: smb5-lib: reset level on removal
    fix fast charge toggle without reboot
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -9321,7 +9321,10 @@ static void op_handle_usb_removal(struct smb_charger *chg)
 	chg->pd_tried_warp_switch = false;
 	op_set_hw_term_charging(false);
 	cancel_delayed_work_sync(&chg->pd_status_check_work);
-
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	chg->ffc_count = 0;
+	set_sdp_current(chg, USBIN_500MA);
+#endif
 	vote(chg->fcc_votable,
 		DEFAULT_VOTER, true, SDP_CURRENT_UA);
 	vote(chg->chg_disable_votable,
